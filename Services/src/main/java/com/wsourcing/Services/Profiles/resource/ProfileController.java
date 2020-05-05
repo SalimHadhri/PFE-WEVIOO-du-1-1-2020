c1e23f1ec3f2cb851eff4d3d155e645c7673cc0d
salim.hadhri <salim.hadhri@esprit.tn>
2020-05-05 13:47:06 +0100

    recherche avancée (range , tunisie on/off , categorie, profile) corrigé

--- Services/src/main/java/com/wsourcing/Services/Profiles/resource/ProfileController.java
+++ Services/src/main/java/com/wsourcing/Services/Profiles/resource/ProfileController.java
@@ -2,12 +2,17 @@ package com.wsourcing.Services.Profiles.resource;
 
 
 
+import ch.qos.logback.core.spi.AbstractComponentTracker;
 import com.sun.org.apache.xpath.internal.operations.Bool;
 import com.wsourcing.Services.Accounts.model.Account;
 import com.wsourcing.Services.Profiles.exception.ProfileNotFoundException;
 import com.wsourcing.Services.Profiles.model.Profile;
+import com.wsourcing.Services.Profiles.model.Skill2;
 import com.wsourcing.Services.Profiles.repository.ProfileRepository;
+import io.micrometer.core.instrument.Tags;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
@@ -89,9 +94,9 @@ public class ProfileController {
     }
 
 
-
     @GetMapping(value = "/SearchAll/{min}/{max}/{tunisia}/{categorie}/{profile}")
-    public List<Profile> SearchAll(@PathVariable int min, @PathVariable int max, @PathVariable int tunisia,@PathVariable int categorie,@PathVariable String profile) {
+    public List<Profile> SearchAll(@PathVariable int min, @PathVariable int max, @PathVariable int tunisia, @PathVariable int categorie
+            , @PathVariable String profile) {
 
 
         List<Profile> profilesRange = experienceRange(min, max);
@@ -103,89 +108,187 @@ public class ProfileController {
         }
         List<Profile> profilesRTNCategorie = new ArrayList<>();
 
-        if(categorie==0){
-            profilesRTNCategorie= profilesRTunisia ;
-        }
-        else{
+        if (categorie == 0) {
+            profilesRTNCategorie = profilesRTunisia;
+        } else {
 
 
-        for (int i = 0; i < profilesRTunisia.size(); i++) {
-            if (profilesRTunisia.get(i).getNew_features().getCateg_seniority() == categorie) {
-                profilesRTNCategorie.add(profilesRTunisia.get(i));
+            for (int i = 0; i < profilesRTunisia.size(); i++) {
+                if (profilesRTunisia.get(i).getNew_features().getCateg_seniority() == categorie) {
+                    profilesRTNCategorie.add(profilesRTunisia.get(i));
+                }
             }
         }
-        }
 
-        List<Profile> profilesRTNCProIngenieur = new ArrayList<>();
-        List<Profile> profilesRTNCProDeveloper = new ArrayList<>();
-        List<Profile> profilesRTNCProManagerTechnique = new ArrayList<>();
-        List<Profile> profilesRTNCProChefDeProjet = new ArrayList<>();
-        List<Profile> profilesRTNCProReferencTechnique = new ArrayList<>();
+        List<Profile> profilesRTNCProTechnicalLead = new ArrayList<>();
+        List<Profile> profilesRTNCProJavaJee = new ArrayList<>();
+        List<Profile> profilesRTNCProDupral = new ArrayList<>();
+        List<Profile> profilesRTNCProProductOwner = new ArrayList<>();
+        List<Profile> profilesRTNCProWebBackEnd = new ArrayList<>();
+        List<Profile> profilesRTNCProFullStackJs = new ArrayList<>();
+        List<Profile> profilesRTNCProEmbarqueMiddleWare = new ArrayList<>();
+        List<Profile> profilesRTNCProPhpSymphony = new ArrayList<>();
 
-        for(int i=0; i<profilesRTNCategorie.size();i++) {
-            if (profilesRTNCategorie.get(i).getHeadline().contains("Ingénieur") || profilesRTNCategorie.get(i).getHeadline().contains("ingénieur")
+
+        for (int i = 0; i < profilesRTNCategorie.size(); i++) {
+            if (profilesRTNCategorie.get(i).getNew_features().getFinal_best_profile().contains("Technical Lead/ Architecte JEE")
+                    /*|| profilesRTNCategorie.get(i).getHeadline().contains("ingénieur")
                     || profilesRTNCategorie.get(i).getHeadline().contains("engineer")
-                    || profilesRTNCategorie.get(i).getHeadline().contains("Engineer")) {
+                    || profilesRTNCategorie.get(i).getHeadline().contains("Engineer")  */)  {
 
-                profilesRTNCProIngenieur.add(profilesRTNCategorie.get(i));
+                profilesRTNCProTechnicalLead.add(profilesRTNCategorie.get(i));
             }
         }
 
-            for(int j=0; j<profilesRTNCategorie.size();j++){
-                if(profilesRTNCategorie.get(j).getHeadline().contains("Développeur") || profilesRTNCategorie.get(j).getHeadline().contains("développeur")
-                        || profilesRTNCategorie.get(j).getHeadline().contains("developer")||profilesRTNCategorie.get(j).getHeadline().contains("Developer")){
+        for (int j = 0; j < profilesRTNCategorie.size(); j++) {
+            if (profilesRTNCategorie.get(j).getNew_features().getFinal_best_profile().contains("JAVA/JEE")
+                   /* || profilesRTNCategorie.get(j).getHeadline().contains("développeur")
+                    || profilesRTNCategorie.get(j).getHeadline().contains("developer") ||
+                    profilesRTNCategorie.get(j).getHeadline().contains("Developer")*/) {
 
-                    profilesRTNCProDeveloper.add(profilesRTNCategorie.get(j)) ;
-                }
+                profilesRTNCProJavaJee.add(profilesRTNCategorie.get(j));
+            }
         }
-            for(int e=0; e<profilesRTNCategorie.size();e++){
-                if(profilesRTNCategorie.get(e).getHeadline().contains("Technical manager") || profilesRTNCategorie.get(e).getHeadline().contains("technical manager")
-                        || profilesRTNCategorie.get(e).getHeadline().contains("Technical Manager")
-                        || profilesRTNCategorie.get(e).getHeadline().contains("Manageur technique")||profilesRTNCategorie.get(e).getHeadline().contains("manageur technique")
-                        || profilesRTNCategorie.get(e).getHeadline().contains("Manageur Technique")){
+        for (int e = 0; e < profilesRTNCategorie.size(); e++) {
+            if (profilesRTNCategorie.get(e).getNew_features().getFinal_best_profile().contains("DRUPAL")
+                    /*|| profilesRTNCategorie.get(e).getHeadline().contains("technical manager")
+                    || profilesRTNCategorie.get(e).getHeadline().contains("Technical Manager")
+                    || profilesRTNCategorie.get(e).getHeadline().contains("Manageur technique") || profilesRTNCategorie.get(e).getHeadline().contains("manageur technique")
+                    || profilesRTNCategorie.get(e).getHeadline().contains("Manageur Technique")*/) {
 
-                    profilesRTNCProManagerTechnique.add(profilesRTNCategorie.get(e)) ;
+                profilesRTNCProDupral.add(profilesRTNCategorie.get(e));
+            }
+        }
+        for (int f = 0; f < profilesRTNCategorie.size(); f++) {
+            if (profilesRTNCategorie.get(f).getNew_features().getFinal_best_profile().contains("Product Owner")
+                    /*|| profilesRTNCategorie.get(f).getHeadline().contains("chef de projet")
+                    || profilesRTNCategorie.get(f).getHeadline().contains("Chef de Projet")
+                    || profilesRTNCategorie.get(f).getHeadline().contains("Project manager") || profilesRTNCategorie.get(f).getHeadline().contains("project manager")
+                    || profilesRTNCategorie.get(f).getHeadline().contains("Project Manager")*/) {
+
+                profilesRTNCProProductOwner.add(profilesRTNCategorie.get(f));
+            }
+        }
+        for (int g = 0; g < profilesRTNCategorie.size(); g++) {
+            if (profilesRTNCategorie.get(g).getNew_features().getFinal_best_profile().contains("Web Back-End")
+                    /*|| profilesRTNCategorie.get(g).getHeadline().contains("référent technique")
+                    || profilesRTNCategorie.get(g).getHeadline().contains("Référent Technique")
+                    || profilesRTNCategorie.get(g).getHeadline().contains("Technical referent") || profilesRTNCategorie.get(g).getHeadline().contains("technical referent")
+                    || profilesRTNCategorie.get(g).getHeadline().contains("Technical Referent")*/) {
+
+                profilesRTNCProWebBackEnd.add(profilesRTNCategorie.get(g));
+            }
+        }
+        for (int x = 0; x < profilesRTNCategorie.size(); x++) {
+            if (profilesRTNCategorie.get(x).getNew_features().getFinal_best_profile().contains("FullStack JS")
+                    /*|| profilesRTNCategorie.get(g).getHeadline().contains("référent technique")
+                    || profilesRTNCategorie.get(g).getHeadline().contains("Référent Technique")
+                    || profilesRTNCategorie.get(g).getHeadline().contains("Technical referent") || profilesRTNCategorie.get(g).getHeadline().contains("technical referent")
+                    || profilesRTNCategorie.get(g).getHeadline().contains("Technical Referent")*/) {
+
+                profilesRTNCProFullStackJs.add(profilesRTNCategorie.get(x));
+            }
+        }
+        for (int y = 0; y < profilesRTNCategorie.size(); y++) {
+            if (profilesRTNCategorie.get(y).getNew_features().getFinal_best_profile().contains("Embarqué Middleware")
+                    /*|| profilesRTNCategorie.get(g).getHeadline().contains("référent technique")
+                    || profilesRTNCategorie.get(g).getHeadline().contains("Référent Technique")
+                    || profilesRTNCategorie.get(g).getHeadline().contains("Technical referent") || profilesRTNCategorie.get(g).getHeadline().contains("technical referent")
+                    || profilesRTNCategorie.get(g).getHeadline().contains("Technical Referent")*/) {
+
+                profilesRTNCProEmbarqueMiddleWare.add(profilesRTNCategorie.get(y));
+            }
+        }
+        for (int z = 0; z < profilesRTNCategorie.size(); z++) {
+            if (profilesRTNCategorie.get(z).getNew_features().getFinal_best_profile().contains("PHP/Symfony")
+                    /*|| profilesRTNCategorie.get(g).getHeadline().contains("référent technique")
+                    || profilesRTNCategorie.get(g).getHeadline().contains("Référent Technique")
+                    || profilesRTNCategorie.get(g).getHeadline().contains("Technical referent") || profilesRTNCategorie.get(g).getHeadline().contains("technical referent")
+                    || profilesRTNCategorie.get(g).getHeadline().contains("Technical Referent")*/) {
+
+                profilesRTNCProPhpSymphony.add(profilesRTNCategorie.get(z));
+            }
+        }
+
+        List<Profile> profilesRTNCProfile = new ArrayList<>();
+
+      //  String profile1 =profile;
+
+        //if (profile.contains("/")){
+           // String profile1=profile.replace(" ","/") ;
+       // }
+
+        if (profile.equals("Technical Lead  Architecte JEE")) {
+            profilesRTNCProfile = profilesRTNCProTechnicalLead;
+        }
+        if (profile.equals("JAVA JEE")) {
+            profilesRTNCProfile = profilesRTNCProJavaJee;
+        }
+        if (profile.equals("DRUPAL")) {
+            profilesRTNCProfile = profilesRTNCProDupral;
+        }
+        if (profile.equals("Product Owner")) {
+            profilesRTNCProfile = profilesRTNCProProductOwner;
+        }
+        if (profile.equals("Web Back-End")) {
+            profilesRTNCProfile = profilesRTNCProWebBackEnd;
+        }
+        if (profile.equals("FullStack JS")) {
+            profilesRTNCProfile = profilesRTNCProFullStackJs;
+        }
+        if (profile.equals("Embarqué Middleware")) {
+            profilesRTNCProfile = profilesRTNCProEmbarqueMiddleWare;
+        }
+        if (profile.equals("PHP Symfony")) {
+            profilesRTNCProfile = profilesRTNCProPhpSymphony;
+        }
+/*
+        for(int i=0;i<profilesRTNCProfile.size();i++){
+
+            List<Skill2> skill2s = profilesRTNCProfile.get(i).getSkills() ;
+                for(int j=0;j<skill2s.size();j++){
+                    Query query = new Query();
+                    query.addCriteria(Criteria.where(skill2s.get(j).getName()).regex(skill1));
+                    profilesRTNCProfileSkill1.add(profilesRTNCProfile.get(i)) ;
                 }
-            }
-            for(int f=0; f<profilesRTNCategorie.size();f++){
-                if(profilesRTNCategorie.get(f).getHeadline().contains("Chef de projet") || profilesRTNCategorie.get(f).getHeadline().contains("chef de projet")
-                        || profilesRTNCategorie.get(f).getHeadline().contains("Chef de Projet")
-                        || profilesRTNCategorie.get(f).getHeadline().contains("Project manager")||profilesRTNCategorie.get(f).getHeadline().contains("project manager")
-                        || profilesRTNCategorie.get(f).getHeadline().contains("Project Manager")){
+            String X = profilesRTNCProfile.get(i).getSkills().
+            Query query = new Query();
+            query.addCriteria(Criteria.where(X).regex(skill1));
 
-                    profilesRTNCProChefDeProjet.add(profilesRTNCategorie.get(f)) ;
-                }
-            }
-            for(int g=0; g<profilesRTNCategorie.size();g++){
-                if(profilesRTNCategorie.get(g).getHeadline().contains("Référent technique") || profilesRTNCategorie.get(g).getHeadline().contains("référent technique")
-                        ||profilesRTNCategorie.get(g).getHeadline().contains("Référent Technique")
-                        || profilesRTNCategorie.get(g).getHeadline().contains("Technical referent")||profilesRTNCategorie.get(g).getHeadline().contains("technical referent")
-                        ||profilesRTNCategorie.get(g).getHeadline().contains("Technical Referent")){
+        }*/
 
-                    profilesRTNCProReferencTechnique.add(profilesRTNCategorie.get(g)) ;
-                }
-            }
-            List<Profile> profilesRTNCProfile = new ArrayList<>();
-            if (profile.equals("Engineer")){
-                profilesRTNCProfile=profilesRTNCProIngenieur ;
-            }
-            if (profile.equals("Developer")){
-                profilesRTNCProfile=profilesRTNCProDeveloper ;
-            }
-            if (profile.equals("Technical Manager")){
-                profilesRTNCProfile=profilesRTNCProManagerTechnique ;
-            }
-            if (profile.equals("Project Manager")){
-                profilesRTNCProfile=profilesRTNCProChefDeProjet ;
-            }
-            if (profile.equals("Technical Referent")){
-                profilesRTNCProfile=profilesRTNCProReferencTechnique ;
-            }
+      /*  List<Profile> profilesRTNCProfileSkill1 = new ArrayList<>();
+        for (int i = 0; i < profilesRTNCProfile.size(); i++) {
+            List<Skill2> skill2s = profilesRTNCProfile.get(i).getSkills();
+           // boolean exist = false;
 
+            for (int j = 0; j < skill2s.size(); j++) {
+                Query query = new Query();
+                query.addCriteria(Criteria.where(skill2s.get(j).getName()).regex(skill1));
+                
+            }*/
+            // Query profile1 = query.addCriteria(Criteria.where(skill2s.get(j).getName()).regex(skill1));
+            //.getClass().cast(profilesRTNCProfile.get(i)) ;
 
-        return profilesRTNCProfile;
+            //  Skill2 skill2 = new Skill2() ;
+            //   skill2.setName(skill1);
+            //  query.addCriteria(Criteria.where(profilesRTNCProfile.get(i).getSkills()).regex(skill2) ) ;
+            // if(query.addCriteria(Criteria.where(profilesRTNCProfile.get(i).getSkills().get(0).getName()).regex(skill1)){
+            //   exist=true ;
+            // }
+            //  }
+            // if (exist==true){
+            //    profilesRTNCProfileSkill1.add(profilesRTNCProfile.get(i)) ;
+            // }
+            // }
+            //  Query query = new Query();
+            //  query.addCriteria(Criteria.where("a").regex(skill1));
+
+        //}
+
+        return profilesRTNCProfile ;
+
     }
+
 }
-
-
 
